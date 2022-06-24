4b5a64ccff2f9730e60525c79be2275057df0459
ZorgZorgen <wriedel13@gmail.com>
2022-06-23 18:06:30 -0700

    Add files via upload

---
+++ checlist.js
@@ -0,0 +1,8 @@
+const asociacion = (llave, valor, objeto) => {
+    objeto[llave] = valor;
+  };
+  
+  const persona = { nombre: 'Wa;ter' };
+  const resultado = asociacion('tamanoPie', 400, persona);
+  
+  console.log({ [persona], resultado });
